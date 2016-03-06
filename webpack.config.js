3bb04814f2f144f9d2773b528b736d2d4296495e
= <kristoffer@kristofferhebert.com>
2016-03-05 18:26:58 -0800

    feat: adding tweak for api

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+entry: [
+  'webpack-dev-server/client?http://localhost:8084', // WebpackDevServer host and port
+  'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+  './scripts/index' // Your appʼs entry point
+]
